6925539c7957817c5388d1c0b9b3713c752a93fd
cristianoAlves <cristiano.alves.ca@gmail.com>
2013-01-20 21:32:09 -0200

    adding basic structure.

---
+++ gerenciador_testes/models.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from django.db import models
+
+
+class CasoDeTeste(models.Model):
+    titulo = models.CharField(max_length=200)
+    caminho_sikuli = models.CharField(max_length=200)
+    
+    def __unicode__(self):
+        return self.titulo
+    
+    class Meta:
+        verbose_name = u'Casos de teste'
+        verbose_name_plural = u'Casos de teste'
+    
+class CasoDeTestePasso(models.Model):
+    caso_teste = models.ForeignKey(CasoDeTeste)
+    n_passo = models.IntegerField()
+    desc_passos = models.CharField(max_length=200)
+    
+    class Meta:
+        verbose_name = u'Passos dos casos de testes'
+        verbose_name_plural = u'Passos dos casos de testes'
+
+class Status(models.Model):
+    status = models.BooleanField()
+    
+    def __unicode__(self):
+        return self.status
+    
+    class Meta:
+        verbose_name = u'Status'
+        verbose_name_plural = u'Status'
+    
+class Execucao(models.Model):
+    caso_teste = models.ForeignKey(CasoDeTeste)
+    status = models.ForeignKey(Status)
+    exec_date = models.DateTimeField('data execucao')
+    
+    class Meta:
+        verbose_name = 'Execu��es'
+        verbose_name_plural = 'Execu��es'
