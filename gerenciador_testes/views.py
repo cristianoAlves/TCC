89a2bce562a9fa6cd0fade83e18b0c4f8169de38
cristianoAlves <cristiano.alves.ca@gmail.com>
2013-01-21 21:44:30 -0200

    modificação no layout do index.html

--- gerenciador_testes/views.py
+++ gerenciador_testes/views.py
@@ -1,14 +1,18 @@
 from django.template import Context, loader
+from django.template import RequestContext
 from gerenciador_testes.models import CasoDeTeste, CasoDeTestePasso
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
-def index(RequestContext):
-    listaCasoTestes = CasoDeTeste.objects.all()
-    t = loader.get_template('gerenciador_testes/index.html')
+
+def index(request):
+    listaCasoTestes = CasoDeTeste.objects.all()   
     c = Context({
         'listaCasoTestes': listaCasoTestes,
     })
-    return HttpResponse(t.render(c))
+    return render_to_response('gerenciador_testes/index.html',
+                              c,
+                              context_instance=RequestContext(request))
 
 def detail(request, CasoDeTeste_id):
     listaDePassos = CasoDeTestePasso.objects.filter(caso_teste__exact=CasoDeTeste_id)
