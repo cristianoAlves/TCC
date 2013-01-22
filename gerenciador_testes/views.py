93bcb16cb9f9ac72edc8c9c80a1d833fa599fd08
cristianoAlves <cristiano.alves.ca@gmail.com>
2013-01-22 15:07:37 -0200

    adicionando templates.

--- gerenciador_testes/views.py
+++ gerenciador_testes/views.py
@@ -10,19 +10,21 @@ def index(request):
     c = Context({
         'listaCasoTestes': listaCasoTestes,
     })
-    return render_to_response('gerenciador_testes/index.html',
+    return render_to_response('gerenciador_testes/principal.html',
                               c,
                               context_instance=RequestContext(request))
 
 def detail(request, CasoDeTeste_id):
     listaDePassos = CasoDeTestePasso.objects.filter(caso_teste__exact=CasoDeTeste_id)
     CasoTesteNome = CasoDeTeste.objects.get(pk=CasoDeTeste_id).titulo
-    t = loader.get_template('gerenciador_testes/detail.html')
+
     c = Context({
         'listaDePassos': listaDePassos,
         'CasoTesteNome': CasoTesteNome,
     })
-    return HttpResponse(t.render(c))
+    return render_to_response('gerenciador_testes/detail.html',
+                              c,
+                              context_instance=RequestContext(request))
 
 def passos(request, CasoDeTeste_id):
     return HttpResponse("You're looking at the passos of caso de teste %s." % CasoDeTeste_id)
