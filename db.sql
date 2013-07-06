8f842aaa4ee7b507ed10686d761c4875820eb7f2
cristianoAlves <cristiano.alves.ca@gmail.com>
2013-07-06 10:58:24 -0300

    adicionando db.sql removendo métrica total projetos cadastrados

---
+++ db.sql
@@ -0,0 +1,40 @@
+﻿BEGIN;
+CREATE TABLE gerenciador_testes_casodeteste (
+    id integer NOT NULL PRIMARY KEY,
+    titulo varchar(200) NOT NULL,
+    caminhoSikuli varchar(200) NOT NULL
+);
+CREATE TABLE gerenciador_testes_casodetestepasso (
+    id integer NOT NULL PRIMARY KEY,
+    casoDeTeste_id integer NOT NULL REFERENCES gerenciador_testes_casodeteste (id),
+    nPasso integer NOT NULL,
+    descr varchar(200) NOT NULL,
+    resultExperado varchar(200) NOT NULL
+)
+;
+CREATE TABLE gerenciador_testes_projeto (
+    id integer NOT NULL PRIMARY KEY,
+    nomeProjeto varchar(200) NOT NULL,
+    dataAbertura date NOT NULL
+)
+;
+CREATE TABLE gerenciador_testes_casodetesteemprojeto (
+    id integer NOT NULL PRIMARY KEY,
+    projeto_id integer NOT NULL REFERENCES gerenciador_testes_projeto (id),
+    casoDeTeste_id integer NOT NULL REFERENCES gerenciador_testes_casodeteste (id)
+)
+;
+CREATE TABLE gerenciador_testes_testset (
+    id integer NOT NULL PRIMARY KEY,
+    nome varchar(200) NOT NULL,
+    dataAbertura date NOT NULL,
+    dataFechamento date
+)
+;
+CREATE TABLE gerenciador_testes_casodetestevstestset (
+    id integer NOT NULL PRIMARY KEY,
+    casoDeTeste_id integer NOT NULL REFERENCES gerenciador_testes_casodeteste (id),
+    testSet_id integer NOT NULL REFERENCES gerenciador_testes_testset (id)
+)
+;
+COMMIT;
